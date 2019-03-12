6e4906110787b7b7a44fd3c2d44563caeff8982d
Theodoros Grammenos <grammenot@csd.auth.gr>
2019-03-12 13:53:59 +0200

    Add search method

--- lektochat_server/lektochat_server.py
+++ lektochat_server/lektochat_server.py
@@ -15,7 +15,7 @@ class LektoServer:
             print(e)
         sql_create_table = """ CREATE TABLE IF NOT EXISTS users (
                                             id integer PRIMARY KEY,
-                                            username text NOT NULL,
+                                            username text NOT NULL UNIQUE,
                                             ip text NOT NULL,
                                             connectiontime integer
                                         ); """
@@ -25,6 +25,7 @@ class LektoServer:
             self.cursor.execute(sql_create_table)
 
     def newconnection(self, username, ip):
+        # Creates a new entry in the database for a user
         try:
             sql_query = """INSERT INTO users(id,username,ip,connectiontime) 
                            VALUES(NULL,?,?,strftime('%s','now'));"""
@@ -43,7 +44,23 @@ class LektoServer:
         self.cursor.execute(sql_query, (results[0][0],))
         self.database.commit()
 
+    def search(self, username):
+        # Get the IP of the user that matches the given ID
+        # If they do not exist return None
+        sql_query = "SELECT * FROM users WHERE username=?"
+        self.cursor.execute(sql_query, (username,))
+        results = self.cursor.fetchall()
+        # Fetchall return the matching rows as a list of tuples
+        if results is not []:
+            # The length of the list must be 1 as usernames are unique
+            return results[0][2]  # The IP is the third entry in a row
+        else:
+            return None
+
     # TODO: Method to get all usernames and IPs
+    # TODO: Create username index for faster searching
+    # TODO: Instead of using usernames to ID users, use UUIDs
+    # TODO: Use connection time to identify if user is the same(client side work required)
     # TODO: Add checks
     # TODO: Add authentication (?????)
-    # TODO: Instead of deleting disconnected users "deactivate" them (move to other DB or sth)
+    # TODO: Instead of deleting disconnected users "deactivate" them (move to other table or sth)
