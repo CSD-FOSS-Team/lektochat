61abf223565b5c1b1f5c37ddf44de1838cce1f13
Fantomas4 <Fantomas4@users.noreply.github.com>
2019-03-21 17:02:51 +0200

    Added class files and created short class descriptions.

---
+++ app.py
@@ -0,0 +1,7 @@
+
+class App:
+    # This class will contain the following objects:
+    # - CLI
+    # - ConnectionHandler
+    # - ConnectedClient
+    # - Config (objects)
---
+++ config.py
@@ -0,0 +1,8 @@
+
+
+class Config:
+    # This class will contain the following fields:
+    # - port range
+    # - selected port
+    # - nickname (user)
+    # - nicknames list
