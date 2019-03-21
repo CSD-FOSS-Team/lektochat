93917899ad2ae9bd9f2f414cfca8223ab4a750fd
xlxs4 <xlxs4@protonmail.ch>
2019-03-21 17:37:48 +0200

    Tide up

--- connection.py
+++ connection.py
@@ -1,27 +1,18 @@
-
-
 class ConnectionHandler:
-    # This class will contain the following methods:\
+    # This class will contain the following:
     # - handshakeListener()
     # - connectToServer()
     # - createConnectedClient
 
 
-
-
-
-
 class ConnectedClient:
-    # This class will contain the following methods:
+    # This class will contain the following:
     # send()
     # receive()
 
 
-
-
-
 class Server:
-    # This class  will contain the following fields:
+    # This class  will contain the following:
     # - IP
     # - port
     # - info (struct)
