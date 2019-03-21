681a7689220faa82d82b996b2fcdf05d304e117a
Fantomas4 <Fantomas4@users.noreply.github.com>
2019-03-21 17:02:51 +0200

    Added class files and created short class descriptions.

---
+++ connection.py
@@ -0,0 +1,27 @@
+
+
+class ConnectionHandler:
+    # This class will contain the following methods:\
+    # - handshakeListener()
+    # - connectToServer()
+    # - createConnectedClient
+
+
+
+
+
+
+class ConnectedClient:
+    # This class will contain the following methods:
+    # send()
+    # receive()
+
+
+
+
+
+class Server:
+    # This class  will contain the following fields:
+    # - IP
+    # - port
+    # - info (struct)
