f678bbb552e6b525be9b94b602081a5416f5df1e
Anastasios Simeonidis <anastasios2014@gmail.com>
2019-04-18 19:46:38 +0300

    Split to modules.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="lektochat",
+    version="0.0.1",
+    author="CSD FOSS Team",
+    description="TODO",
+    url="https://github.com/CSD-FOSS-Team/lektochat",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={
+    },
+    classfilters=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Not decided yet",
+        "Operating System :: POSIX",
+    ],
+)
